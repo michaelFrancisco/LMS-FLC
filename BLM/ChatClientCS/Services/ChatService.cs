d34d8ce78996f6ba22fbd6af8d083875cee02a04
reynaldodantes123 <reynaldodantes123@gmail.com>
2020-03-08 10:53:33 +0800

    new reports trial version

--- BLM/ChatClientCS/Services/ChatService.cs
+++ BLM/ChatClientCS/Services/ChatService.cs
@@ -23,7 +23,7 @@ namespace BLM.ChatClientCS.Services
 
         private IHubProxy hubProxy;
         private HubConnection connection;
-        private string url = "http://*:8080/signalchat";
+        private string url = "http://192.168.254.106:420/signalchat";
 
         public async Task ConnectAsync()
         {
